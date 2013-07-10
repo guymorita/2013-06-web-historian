a5b6b3c773218ffb2b43fdafbb98e0bfb6412721
Hack Reactor Students <github@hackreactor.com>
2013-07-10 12:44:04 -0700

    updated spec for ports and folders

--- spec/ServerSpec.js
+++ spec/ServerSpec.js
@@ -1,6 +1,7 @@
-var handler = require("../web/request-handler");
-handler.datadir = __dirname + "testdata/sites.txt";
+var handler = require("../web/compiled/src/request-handler");
+handler.datadir = __dirname + "/testdata/sites.txt";
 var stubs = require("./helpers/stubs");
+var fs = require('fs');
 var res;
 
 // allows us to run tests async
@@ -16,7 +17,7 @@ beforeEach(function(){
 describe("Node Server Request Listener Function", function() {
 
   it("Should answer GET requests for /", function() {
-    var req = new stubs.Request("http://127.0.0.1:8080/", "GET");
+    var req = new stubs.Request("http://127.0.0.1:8081/", "GET");
     handler.handleRequest(req, res);
     async(function(){
       expect(res._responseCode).toEqual(200);
@@ -27,7 +28,7 @@ describe("Node Server Request Listener Function", function() {
 
   it("Should answer GET requests for archived websites", function() {
     var fixtureName = "www.google.com";
-    var req = new stubs.Request("http://127.0.0.1:8080/" + fixtureName, "GET");
+    var req = new stubs.Request("http://127.0.0.1:8081/" + fixtureName, "GET");
     handler.handleRequest(req, res);
     async(function(){
       expect(res._responseCode).toEqual(200);
@@ -40,18 +41,18 @@ describe("Node Server Request Listener Function", function() {
     fs.writeFileSync(handler.datadir, ""); // reset the test file
 
     var url = "www.example.com";
-    var req = new stubs.Request("http://127.0.0.1:8080/", "POST", {url: url});
+    var req = new stubs.Request("http://127.0.0.1:8081/", "POST", {url: url});
 
     handler.handleRequest(req, res);
 
-    var fileContents = fs.readFileSync(handler.datadir);
+    var fileContents = fs.readFileSync(handler.datadir, 'utf8');
     expect(res._responseCode).toEqual(302);
     expect(fileContents).toEqual(url + "\n");
     expect(res._ended).toEqual(true);
   });
 
   it("Should 404 when asked for a nonexistent file", function() {
-    var req = new stubs.Request("http://127.0.0.1:8080/arglebargle", "GET");
+    var req = new stubs.Request("http://127.0.0.1:8081/arglebargle", "GET");
     handler.handleRequest(req, res);
     async(function() {
       expect(res._responseCode).toEqual(404);
